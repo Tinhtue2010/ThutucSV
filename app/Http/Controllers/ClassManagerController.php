5409db79fa0ef4157bc6db853f202c51ba67025b
ChinoTheGod <meodihia756@gmail.com>
2024-04-08 20:22:51 +0700

    Modified controllers

--- app/Http/Controllers/ClassManagerController.php
+++ app/Http/Controllers/ClassManagerController.php
@@ -3,6 +3,7 @@
     namespace App\Http\Controllers;
 
     use App\Models\Lop;
+    use Illuminate\Database\QueryException;
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Auth;
 
@@ -17,15 +18,17 @@
         {
             $query = Lop::query();
 
-            // if (isset($request->status_error)) {
-            //     if ($request->status_error != 'all') {
-            //         if ($request->status_error == 0) {
-            //             $query->whereNull('return_type');
-            //         } else {
-            //             $query->where('return_type', $request->status_error);
-            //         }
-            //     }
-            // }
+            $query->when(
+                $request->has('status_error')
+                && $request->status_error !== 'all',
+                function ($query) use ($request) {
+                    $query->where(
+                        $request->status_error === 0 ? 'return_type' : null,
+                        $request->status_error === 0 ? null
+                            : $request->status_error
+                    );
+                }
+            );
 
             $data = $this->queryPagination($request, $query, []);
 
@@ -54,6 +57,33 @@
 
         function create(Request $request)
         {
+            try {
+                Lop::create($request->only([
+                    'name',
+                    'nganh',
+                    'khoa_id'
+                ]));
 
+                return true;
+            } catch (QueryException $e) {
+                abort(404);
+            }
+        }
+
+        public function update(Request $request, $id)
+        {
+            $lop = Lop::find($id);
+
+            if (!$lop) {
+                return response()->json([
+                    'message' => 'Not found',
+                ], 404);
+            }
+
+            return $lop->update($request->only([
+                'name',
+                'nganh',
+                'khoa_id'
+            ]));
         }
     }
