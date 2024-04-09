0c6d10af926704b89957ccbf003e131b98777eaf
phamhongdev <hongichik321@gmail.com>
2024-04-09 20:25:58 +0700

    update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,24 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Lop;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class StudentController extends Controller
 {
-    function index() {
+    function index()
+    {
         $user = Auth::user();
-        $student = Student::leftJoin('lops','students.lop_id','=','lops.id')
-        ->leftJoin('khoas','lops.khoa_id','=','khoas.id')
-        ->select('students.*','lops.name as lop_name','khoas.name as khoa_name')
-        ->where('students.id',$user->student_id)->first();
+        $student = Student::leftJoin('lops', 'students.lop_id', '=', 'lops.id')
+            ->leftJoin('khoas', 'lops.khoa_id', '=', 'khoas.id')
+            ->select('students.*', 'lops.name as lop_name', 'khoas.name as khoa_name')
+            ->where('students.id', $user->student_id)->first();
         $studentArray = $student->toArray();
         $totalFields = count($studentArray);
         $nonNullFields = count(array_filter($studentArray));
-        
-        $percentage = intval(($nonNullFields / $totalFields) * 100);      
-        
-        return view('student.index', ['percent'=>$percentage,'student'=>$student]);
+
+        $percentage = intval(($nonNullFields / $totalFields) * 100);
+        $lops = Lop::get();
+
+        return view('student.index', ['percent' => $percentage, 'student' => $student, 'lops' => $lops]);
+    }
+
+    public function getDataInfo()
+    {
+        $user = Auth::user();
+        try {
+            $error = Student::findOrFail($user->student_id);
+
+            return $error;
+        } catch (QueryException $e) {
+            abort(404);
+        }
+    }
+
+    public function update(Request $request) {
+        $user = Auth::user();
+        $student = Student::find($user->student_id);
+
+        return $student->update($request->only([
+            'full_name',
+            'date_of_birth',
+            'phone',
+            'email',
+            'cmnd',
+            'date_range_cmnd'
+        ]));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-    use App\Http\Controllers\ApproveController;
-    use App\Http\Controllers\AuthController;
-    use App\Http\Controllers\ClassManagerController;
-    use App\Http\Controllers\KhoaManagerController;
-    use App\Http\Controllers\StudentController;
-    use App\Http\Controllers\StudentManagerController;
-    use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApproveController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClassManagerController;
+use App\Http\Controllers\KhoaManagerController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\StudentManagerController;
+use Illuminate\Support\Facades\Route;
 
-    /*
+/*
     |--------------------------------------------------------------------------
     | Web Routes
     |--------------------------------------------------------------------------
@@ -19,97 +19,132 @@
     |
     */
 
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/check_login', [AuthController::class, 'checkLogin'])
-        ->name('checkLogin');
-    Route::get('/log-out', [AuthController::class, 'logout'])->name('logout');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/check_login', [AuthController::class, 'checkLogin'])
+    ->name('checkLogin');
+Route::get('/log-out', [AuthController::class, 'logout'])->name('logout');
 
-    Route::group(['middleware' => ['auth']], function () {
-        // chuyển hướng nếu vô /
-        Route::get('/', function () {
-            if (Role(1)) {
-                return redirect()->route('student.info');
-            }
-            return redirect()->route('approve.index');
-        })->name('home');
-        // chỉ sinh viên được vào
-        Route::middleware('role:student')->group(function () {
-            Route::name('student.')->group(function () {
-                Route::get('/student-info', [StudentController::class, 'index'])
-                    ->name('info');
-            });
+Route::group(['middleware' => ['auth']], function () {
+    // chuyển hướng nếu vô /
+    Route::get('/', function () {
+        if (Role(1)) {
+            return redirect()->route('student.info');
+        }
+        return redirect()->route('approve.index');
+    })->name('home');
+    // chỉ sinh viên được vào
+    Route::middleware('role:student')->group(function () {
+        Route::name('student.')->group(function () {
+            Route::get('/student-info', [StudentController::class, 'index'])
+                ->name('info');
+            Route::get('get-data-info', [StudentController::class, 'getDataInfo'])
+                ->name('getDataInfo');
+            Route::post('update', [StudentController::class, 'update'])
+                ->name('update');
         });
 
-        //tất cả được vào trừ sinh viên
-        Route::middleware('role:notStudent')->group(function () {
-            Route::name('approve.')->prefix('approve')->group(function () {
-                Route::get('/', [ApproveController::class, 'index'])
-                    ->name('index');
-            });
-        });
-
-        // quyền admin hoặc là quyền bên phòng đào tạo được vào
-        Route::middleware('role:studentManager')->name('studentManager.')
-            ->prefix('student-manager')->group(function () {
-                Route::get('/', [StudentManagerController::class, 'index'])
-                    ->name('index');
-                Route::get('get-data',
-                    [StudentManagerController::class, 'getData'])
-                    ->name('getData');
-                Route::get('get-data/{id?}',
-                    [StudentManagerController::class, 'getDataChild'])
-                    ->name('getDataChild');
-                Route::get('detele/{id?}',
-                    [StudentManagerController::class, 'detele'])
-                    ->name('detele');
-                Route::post('create',
-                    [StudentManagerController::class, 'create'])
-                    ->name('create');
-                Route::post('update/{id?}',
-                    [StudentManagerController::class, 'update'])
-                    ->name('update');
-                Route::post('import-file',
-                    [StudentManagerController::class, 'importFile'])
-                    ->name('importFile');
-            });
-
-        Route::middleware('role:khoaManager')->name('khoaManager.')
-            ->prefix('khoa-manager')->group(function () {
-                Route::get('/', [KhoaManagerController::class, 'index'])
-                    ->name('index');
-                Route::get('get-data',
-                    [KhoaManagerController::class, 'getData'])
-                    ->name('getData');
-                Route::get('get-data/{id?}',
-                    [KhoaManagerController::class, 'getDataChild'])
-                    ->name('getDataChild');
-                Route::get('detele/{id?}',
-                    [KhoaManagerController::class, 'detele'])
-                    ->name('detele');
-                Route::post('create', [KhoaManagerController::class, 'create'])
-                    ->name('create');
-                Route::post('update/{id?}',
-                    [KhoaManagerController::class, 'update'])
-                    ->name('update');
-            });
-
-        Route::middleware('role:classManager')->name('classManager.')
-            ->prefix('class-manager')->group(function () {
-                Route::get('/', [ClassManagerController::class, 'index'])
-                    ->name('index');
-                Route::get('get-data',
-                    [ClassManagerController::class, 'getData'])
-                    ->name('getData');
-                Route::get('get-data/{id?}',
-                    [ClassManagerController::class, 'getDataChild'])
-                    ->name('getDataChild');
-                Route::get('detele/{id?}',
-                    [ClassManagerController::class, 'detele'])
-                    ->name('detele');
-                Route::post('create', [ClassManagerController::class, 'create'])
-                    ->name('create');
-                Route::post('update/{id?}',
-                    [ClassManagerController::class, 'update'])
-                    ->name('update');
-            });
+        
     });
+
+    //tất cả được vào trừ sinh viên
+    Route::middleware('role:notStudent')->group(function () {
+        Route::name('approve.')->prefix('approve')->group(function () {
+            Route::get('/', [ApproveController::class, 'index'])
+                ->name('index');
+        });
+    });
+
+    // quyền admin hoặc là quyền bên phòng đào tạo được vào
+    Route::middleware('role:studentManager')->name('studentManager.')
+        ->prefix('student-manager')->group(function () {
+            Route::get('/', [StudentManagerController::class, 'index'])
+                ->name('index');
+            Route::get(
+                'get-data',
+                [StudentManagerController::class, 'getData']
+            )
+                ->name('getData');
+            Route::get(
+                'get-data/{id?}',
+                [StudentManagerController::class, 'getDataChild']
+            )
+                ->name('getDataChild');
+            Route::get(
+                'detele/{id?}',
+                [StudentManagerController::class, 'detele']
+            )
+                ->name('detele');
+            Route::post(
+                'create',
+                [StudentManagerController::class, 'create']
+            )
+                ->name('create');
+            Route::post(
+                'update/{id?}',
+                [StudentManagerController::class, 'update']
+            )
+                ->name('update');
+            Route::post(
+                'import-file',
+                [StudentManagerController::class, 'importFile']
+            )
+                ->name('importFile');
+        });
+
+    Route::middleware('role:khoaManager')->name('khoaManager.')
+        ->prefix('khoa-manager')->group(function () {
+            Route::get('/', [KhoaManagerController::class, 'index'])
+                ->name('index');
+            Route::get(
+                'get-data',
+                [KhoaManagerController::class, 'getData']
+            )
+                ->name('getData');
+            Route::get(
+                'get-data/{id?}',
+                [KhoaManagerController::class, 'getDataChild']
+            )
+                ->name('getDataChild');
+            Route::get(
+                'detele/{id?}',
+                [KhoaManagerController::class, 'detele']
+            )
+                ->name('detele');
+            Route::post('create', [KhoaManagerController::class, 'create'])
+                ->name('create');
+            Route::post(
+                'update/{id?}',
+                [KhoaManagerController::class, 'update']
+            )
+                ->name('update');
+        });
+
+
+    Route::middleware('role:classManager')->name('classManager.')
+        ->prefix('class-manager')->group(function () {
+            Route::get('/', [ClassManagerController::class, 'index'])
+                ->name('index');
+            Route::get(
+                'get-data',
+                [ClassManagerController::class, 'getData']
+            )
+                ->name('getData');
+            Route::get(
+                'get-data/{id?}',
+                [ClassManagerController::class, 'getDataChild']
+            )
+                ->name('getDataChild');
+            Route::get(
+                'detele/{id?}',
+                [ClassManagerController::class, 'detele']
+            )
+                ->name('detele');
+            Route::post('create', [ClassManagerController::class, 'create'])
+                ->name('create');
+            Route::post(
+                'update/{id?}',
+                [ClassManagerController::class, 'update']
+            )
+                ->name('update');
+        });
+});
