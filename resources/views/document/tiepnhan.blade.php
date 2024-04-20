2ab6642ba408ddb5e0becdffb191f7d268a65bfe
Zord <quangsang.red@gmail.com>
2024-04-20 22:34:58 +0700

    add doc for basis CTSV (Officer side)

---
+++ resources/views/document/tiepnhan.blade.php
@@ -0,0 +1,90 @@
+<div class="A4 mx-auto d-flex flex-column">
+    <div class="d-flex flex-row justify-content-between">
+      <div>
+        <div class="text-center">TRƯỜNG ĐẠI HỌC HẠ LONG</div>
+        <div class="text-center fw-bold">PHÒNG CTCT, HT&SV</div>
+        <div
+          style="
+            width: 94px;
+            height: 1px;
+            background-color: black;
+            margin-top: -1px;
+            margin-left: auto;
+            margin-right: auto;
+          "
+        ></div>
+      </div>
+      <div>
+        <div class="text-center">CỘNG HOÀ XÃ HỘI CHỦ NGHĨA VIỆT NAM</div>
+        <div class="text-center fw-bold">Độc lập - Tự do - Hạnh phúc</div>
+        <div
+          style="
+            width: 203px;
+            height: 1px;
+            background-color: black;
+            margin-top: -1px;
+            margin-left: auto;
+            margin-right: auto;
+          "
+        ></div>
+        <div class="text-center fst-italic mt-1">
+          Quảng Ninh, ngày ?? tháng ?? năm ????
+        </div>
+      </div>
+    </div>
+  
+    <div class="text-center fw-bold" style="margin-top: 35px">
+      PHIẾU TIẾP NHẬN HỒ SƠ VÀ HẸN TRẢ KẾT QUẢ
+    </div>
+    <div class="text-center">Mã hồ sơ: ...................</div>
+    <div class="d-flex flex-column">
+      <p>
+        Phòng Công tác chính trị, Quản lý và Hỗ trợ sinh viên đã tiếp nhận hồ sơ
+        của: <br />
+        Sinh viên: Phạm Nguyên Hồng
+        <br />
+        Số định danh cá nhân/CMND: ......................................... Ngày
+        cấp:99/99/9999 <br />
+        SĐT: 123124123 &nbsp; Email: whoami@mail.com
+        <br />
+        Nội dung yêu cầu giải quyết: Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Harum maxime iusto voluptatem, nemo qui cumque amet?
+        Laudantium quis laborum iste veniam architecto voluptas a facilis,
+        repellat blanditiis repudiandae, soluta consequuntur.
+        <br />
+        Thành phần hồ sơ nộp gồm:
+      </p>
+      <table class="table lh-1">
+        <tr class="text-center">
+          <th scope="col" style="width: 50px">TT</th>
+          <th scope="col">Tên giấy tờ</th>
+          <th scope="col" style="width: 160px">
+            <div>Hình thức</div>
+            <div>(bản chinh,bản sao hoặc bản chụp)</div>
+          </th>
+          <th scope="col" style="width: 120px">Ghi chú</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+      <p>
+        Thời gian nhận hồ sơ 99 giờ 99 phút, ngày 99/99/9999<br />
+        Thời gian trả kết quả giải quyết hồ sơ: 99 giờ 99 phút, ngày 99/99/9999
+        <br />
+        Đăng ký nhận kêt quả tại:ABC
+      </p>
+    </div>
+    <div class="d-flex flex-row justify-content-between px-5 mt-1">
+      <div class="d-flex flex-column justify-content-end text-center"></div>
+      <div class="text-center">
+        <div class="fst-italic">Uông Bí, ngày 15 tháng 12 năm 2023</div>
+        <div class="fw-bold">NGƯỜI TIẾP NHẬN HỒ SƠ</div>
+        <br />
+      </div>
+    </div>
+  </div>
+  
---
+++ resources/views/document/tuchoi.blade.php
@@ -0,0 +1,70 @@
+<div class="A4 mx-auto d-flex flex-column">
+    <div class="d-flex flex-row justify-content-between">
+        <div>
+            <div class="text-center">TRƯỜNG ĐẠI HỌC HẠ LONG</div>
+            <div class="text-center fw-bold">PHÒNG CTCT, HT&SV</div>
+            <div
+                style="
+            width: 94px;
+            height: 1px;
+            background-color: black;
+            margin-top: -1px;
+            margin-left: auto;
+            margin-right: auto;
+          ">
+            </div>
+        </div>
+        <div>
+            <div class="text-center">CỘNG HOÀ XÃ HỘI CHỦ NGHĨA VIỆT NAM</div>
+            <div class="text-center fw-bold">Độc lập - Tự do - Hạnh phúc</div>
+            <div
+                style="
+            width: 203px;
+            height: 1px;
+            background-color: black;
+            margin-top: -1px;
+            margin-left: auto;
+            margin-right: auto;
+          ">
+            </div>
+            <div class="text-center fst-italic mt-1">
+                Quảng Ninh, ngày ?? tháng ?? năm ????
+            </div>
+        </div>
+    </div>
+    <div class="text-center fw-bold" style="margin-top: 35px">
+        PHIẾU TỪ CHỐI TIẾP NHẬN, GIẢI QUYẾT HỒ SƠ
+    </div>
+    <div class="d-flex flex-column mt-3">
+        <p class="mb-0">
+            Hồ sơ của sinh viên: Phạm Nguyên Hồng
+            <br />
+            Số định danh cá nhân/CMND: ......................................... Ngày
+            cấp:99/99/9999 <br />
+            SĐT: 123124123 &nbsp; Email: whoami@mail.com
+            <br />
+            Nội dung yêu cầu giải quyết: Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Harum maxime iusto voluptatem, nemo qui cumque amet?
+            Laudantium quis laborum iste veniam architecto voluptas a facilis,
+            repellat blanditiis repudiandae, soluta consequuntur.
+            <br />Qua xem xét hồ sơ, Phòng Công tác chính trị, Quản lý và Hỗ trợ sinh
+            viên thông báo không tiếp nhận, giải quyết hồ sơ này với lý do, cụ thể như
+            sau:
+        </p>
+        <p>
+            &emsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo libero
+            quidem velit unde aliquid accusamus quod, quaerat accusantium ipsum
+            possimus perspiciatis, facilis earum dicta? Suscipit earum cupiditate
+            repellat perferendis voluptatibus. <br />
+            Xin thông báo đến sinh viên………………………………… được biết./
+        </p>
+    </div>
+    <div class="d-flex flex-row justify-content-between px-5 mt-1">
+        <div class="d-flex flex-column justify-content-end text-center"></div>
+        <div class="text-center">
+            <div class="fst-italic">Uông Bí, ngày 15 tháng 12 năm 2023</div>
+            <div class="fw-bold">NGƯỜI TIẾP NHẬN HỒ SƠ</div>
+            <br />
+        </div>
+    </div>
+</div>
