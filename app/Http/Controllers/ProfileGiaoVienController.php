a28d2fb3d710e1e14417e4cedbf737ceb3bf212d
nguyenduy-thanh <nguyenduyduy032004@gmail.com>
2024-06-20 09:19:39 +0700

    update

--- app/Http/Controllers/ProfileGiaoVienController.php
+++ app/Http/Controllers/ProfileGiaoVienController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+
+use APP\Models\Teacher;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileGiaoVienController extends Controller
 {
-    //
+    function index(){
+        $user = Auth::user();
+        $teacher = Teacher::leftJoin('khoas','teachers.khoa_id','=','khoas.id')
+        ->select('teachers.*','khoas.name as khoa_name')
+        ->where('teachers.id',$user->teacher_id)->first();
+        $teacherArray = $teacher->toArray();
+        $totalFields = count($teacherArray);
+        $nonNullFields = count(array_filter($teacherArray));
+
+        $percentage = intval(($nonNullFields / $totalFields) * 100);
+        return view('giao_vien.index',['percent'=>$percentage,'teacher'=>$teacher]);
+    }
+
+    public function getDataInfo()
+    {
+        $user = Auth::user();
+        try {
+            $error = Teacher::findOrFail($user->teacher_id);
+
+            return $error;
+        } catch (QueryException $e) {
+            abort(404);
+        }
+    }
+
+    public function update(Request $request) {
+        $user = Auth::user();
+        $teacher = Teacher::find($user->teacher_id);
+
+        return $teacher->update($request->only([
+            'full_name',
+            'dia_chi',
+            'sdt',
+            'email'
+        ]));
+    }
 }
--- resources/views/giao_vien/index.blade.php
+++ resources/views/giao_vien/index.blade.php
@@ -7,6 +7,180 @@
         <div id="kt_app_content" class="app-content flex-column-fluid">
             <!--begin::Content container-->
             <div id="kt_app_content_container" class="app-container container-fluid">
+                <!-- Name -->
+                <div class="card mb-5 mb-xl-10">
+                    <div class="card-body pt-9 pb-0">
+                        <!--begin::Details-->
+                        <div class="d-flex flex-wrap flex-sm-nowrap">
+                            <!--begin: Pic-->
+                            <div class="me-7 mb-4">
+                                <div class="symbol symbol-100px symbol-lg-160px symbol-fixed">
+                                    <img src="{{ asset('assets/custom/avatar.png') }}" alt="image">
+                                </div>
+                            </div>
+                            <!--end::Pic-->
+                            <!--begin::Info-->
+                            <div class="flex-grow-1">
+                                <!--begin::Title-->
+                                <div class="d-flex justify-content-between align-items-start flex-wrap mb-2">
+                                    <!--begin::User-->
+                                    <div class="d-flex flex-column">
+                                        <!--begin::Name-->
+                                        <div class="d-flex align-items-center mb-2">
+                                            <a href="#" class="text-gray-900 text-hover-primary fs-2 fw-bold me-1">{{ $teacher->full_name }}</a>
+                                        </div>
+                                        <!--end::Name-->
+                                        <!--begin::Info-->
+                                        <div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
+                                            <a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
+                                                <i class="ki-outline ki-abstract-39 fs-4 me-1"></i>{{ $teacher->khoa_name }}</a>
+                                            <a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary mb-2">
+                                                <i class="ki-outline ki-sms fs-4 me-1"></i>{{ $teacher->email }}</a>
+                                        </div>
+                                        <!--end::Info-->
+                                    </div>
+                                    <!--end::User-->
+                                </div>
+                                <!--end::Title-->
+                                <!--begin::Stats-->
+                                <div class="d-flex flex-wrap flex-stack">
+                                    <!--begin::Wrapper-->
+                                    <div class="d-flex flex-column flex-grow-1 pe-8">
+                                        <!--begin::Stats-->
+                                        <div class="d-flex flex-wrap">
+                                            <!--begin::Stat-->
+                                            <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
+                                                <!--begin::Number-->
+                                                <div class="d-flex align-items-center">
+                                                    <i class="ki-outline ki-archive-tick fs-3 text-success me-2"></i>
+                                                    <div class="fs-2 fw-bold counted" data-kt-countup="true" data-kt-countup-value="{{ $student->so_tien }}" data-kt-initialized="1">Chức danh</div>
+                                                </div>
+                                                <!--end::Number-->
+                                                <!--begin::Label-->
+                                                <div class="fw-semibold fs-6 text-gray-400">{{$teacher->chuc_danh}}</div>
+                                                <!--end::Label-->
+                                            </div>
+                                            <!--end::Stat-->
+                                        </div>
+                                        <!--end::Stats-->
+                                    </div>
+                                    <!--end::Wrapper-->
+                                    <!--begin::Progress-->
+                                    <div class="d-flex align-items-center w-200px w-sm-300px flex-column mt-3">
+                                        <div class="d-flex justify-content-between w-100 mt-auto mb-2">
+                                            <span class="fw-semibold fs-6 text-gray-400">Hoàn thành hồ sơ</span>
+                                            <span class="fw-bold fs-6">{{ $percent }}%</span>
+                                        </div>
+                                        <div class="h-5px mx-3 w-100 bg-light mb-3">
+                                            <div class="bg-success rounded h-5px" role="progressbar" style="width: {{ $percent }}%;" aria-valuenow="{{ $percent }}" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                    <!--end::Progress-->
+                                </div>
+                                <!--end::Stats-->
+                            </div>
+                            <!--end::Info-->
+                        </div>
+                        <!--end::Details-->
+                    </div>
+                </div>
+                <!--end::Name-->
+                <!--teacher info-->
+                <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+                    <!--begin::Card header-->
+                    <div class="card-header cursor-pointer">
+                        <!--begin::Card title-->
+                        <div class="card-title m-0">
+                            <h3 class="fw-bold m-0">Thông tin cá nhân</h3>
+                        </div>
+                        <!--end::Card title-->
+                        <!--begin::Action-->
+                        <div onclick="btnEdit()" class="btn btn-sm btn-primary align-self-center">Chỉnh sửa</div>
+                        <!--end::Action-->
+                    </div>
+                    <!--begin::Card header-->
+                    <!--begin::Card body-->
+                    <div class="card-body p-9">
+                        <!--begin::Row-->
+                        <div class="row mb-7">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 fw-semibold text-muted">Họ và tên</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8">
+                                <span class="fw-bold fs-6 text-gray-800">{{ $teacher->full_name }}</span>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Row-->
+                        
+                        <!--begin::Row-->
+                        <div class="row mb-7">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 fw-semibold text-muted">Địa chỉ</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8">
+                                <span class="fw-bold fs-6 text-gray-800">{{ $teacher->dia_chi }}
+                                </span>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Row-->
+                        <!--begin::Row-->
+                        <div class="row mb-7">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 fw-semibold text-muted">Số điện thoại</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8">
+                                <span class="fw-bold fs-6 text-gray-800">{{ $teacher->sdt }}</span>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Row-->
+                        <!--begin::Row-->
+                        <div class="row mb-7">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 fw-semibold text-muted">Email</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8">
+                                <span class="fw-bold fs-6 text-gray-800">{{ $teacher->email }}</span>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Row-->
+                        <!--begin::Row-->
+                        <div class="row mb-7">
+                            <!--begin::Label-->
+                            <label class="col-lg-4 fw-semibold text-muted">Chức danh</label>
+                            <!--end::Label-->
+                            <!--begin::Col-->
+                            <div class="col-lg-8">
+                                <span class="fw-bold fs-6 text-gray-800">{{ $teacher->chuc_danh }}</span>
+                            </div>
+                            <!--end::Col-->
+                        </div>
+                        <!--end::Row-->
+                    </div>
+                    <!--end::Card body-->
+                </div>
+                <!--end::teacher info-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 <!--begin::Products-->
                 <div class="card card-flush">
                     <!--begin::Card header-->
@@ -49,9 +223,6 @@
                                 </select>
                                 <!--end::Select2-->
                             </div>
-@include('common.select_status_ho_so')
-
-
                         </div>
                         <!--end::Card toolbar-->
                     </div>
@@ -125,9 +296,12 @@
         </div>
         <!--end::Content-->
     </div>
+    @include('common.select_status_ho_so')
     @include('giao_vien.table')
     @include('giao_vien.xacnhan',['target'=>'xacnhan'])
     @include('giao_vien.khongxacnhan',['target'=>'khongxacnhan'])
     @include('giao_vien.chitiet',['target'=>'chitiet'])
     @include('giao_vien.tientrinh',['target'=>'tientrinh'])
+    @include('giao_vien.field')
+    @include('giao_vien.validate')
 @endsection
